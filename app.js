67a4e7f6c576f6537570e9bd6e24fffb15ef0121
Jon Nehring <jwn@stonetip.com>
2019-06-12 15:00:36 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+//const WebSocket = require("ws");
+
+//const ws = new WebSocket("wss://echo.websocket.org");
+
+//ws.on("open", function open() {
+//	ws.send("something");
+//});
+
+//ws.on("message", function incoming(data) {
+//	console.log(`message from remote server: ${data}`);
+//});
+
+
+//setTimeout(function () {
+
+//	ws.send("here's another message");
+//}, 2000);
+
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 7856 });
+
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+	});
+
+	ws.send('something');
+});
