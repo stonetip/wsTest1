703945d3d96236e689899e4e1d1599428ddce774
Jon Nehring <jwn@stonetip.com>
2019-06-13 15:26:17 -0600

    Added more diagnostic messages

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ const wss = new WebSocket.Server({ port: 7856 });
 
 console.log(`ws test is running on port ${wss.address().port}`);
 
-let appInstance = wss.on('connection', function connection(ws) {
+let appInstance = wss.on('connection', function connection(ws, req) {
+
+	console.log(`client connected from ${req.connection.remoteAddress}`);
 
 	ws.on('message', function incoming(message) {
 		console.log('received: %s', message);
@@ -40,7 +42,35 @@ let appInstance = wss.on('connection', function connection(ws) {
 		//ws.send(jsonString);
 
 		ws.send(`You sent ${message}`);
+
+		////var debug = { hello: "world" };
+		////var blob = new Blob([JSON.stringify(debug, null, 2)], { type: 'application/json' });
+
+		////ws.send(blob);
 	});
 
 
+	// Demonstrating sending a ping or daemon-style message on a regular basis
+	setInterval(function () {
+
+		const msg = "interval message";
+
+		console.log(`universal: ${msg}`);
+
+		const timestamp = getTimeStamp();
+
+		const msgStr = JSON.stringify({ autoBroadcastMessage: msg, time: timestamp });
+
+		ws.send(msgStr);
+
+	}, 10000);
+
 });
+
+
+// util
+function getTimeStamp() {
+
+	return new Date().toISOString();
+}
+
